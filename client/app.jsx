1e3f5e2ff814977038d51266a9d757496d2a23c0
Chaim Brisk <cbrisk99@gmail.com>
2021-02-10 12:42:20 -0800

    started the app sign in

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Home from './pages/home';
 
 const App = () => {
 
   return <Home />;
 
+  const renderPage = () => {
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {loggedIn ? <Redirect to="/dashboard" /> : <Home />}
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+      </Switch>
+    </Router>
+  }
+
+  if (this.state.isAuthorizing) return null;
+  return (
+    <div className="custom-container">
+      <Header />
+      { this.renderPage()}
+      <Footer token={this.state.token} />
+    </div>
+  );
 }
 
 export default App
--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 require('dotenv/config');
 const express = require('express');
+const pg = require('pg');
+const argon2 = require('argon2');
+const authenticateUser = require('./authenticate-user');
+const authorizationMiddleware = require('./authorization-middleware');
 const staticMiddleware = require('./static-middleware');
 
 const app = express();
@@ -11,6 +15,53 @@ app.use(jsonMiddleware);
 
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL
+});
+
+app.post('/api/sign-up', (req, res, next) => {
+  const { username, password, name } = req.body;
+  if (!username || !password || !name) {
+    throw new ClientError(400, 'name, username, password, and user-type are required fields');
+  }
+  argon2
+    .hash(password)
+    .then(hashedPassword => {
+      const sql = `
+        insert into "users" ("name", "username", "password", "role")
+        values ($1, $2, $3)
+        on conflict ("username")
+        do nothing
+        returning "userId"
+      `;
+      const params = [name, username, hashedPassword, role];
+      return db.query(sql, params)
+        .then(result => {
+          const [userId] = result.rows;
+          if (!userId) {
+            throw new ClientError(400, 'That username is not available.');
+          }
+          return authenticateUser(username, password, db)
+            .then(result => {
+              res.status(201).json(result);
+            });
+        });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+app.post('/api/sign-in', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(401, 'invalid login');
+  }
+  authenticateUser(username, password, db)
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 
